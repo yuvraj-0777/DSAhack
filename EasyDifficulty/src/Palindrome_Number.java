a7c3c9460bd8aadf3e2ccd769aeea522797ba1fb
gulatikeshav <133638164+gulatikeshav@users.noreply.github.com>
2023-10-10 12:23:51 +0530

    Added a program Palindrome_Number.java

---
+++ EasyDifficulty/src/Palindrome_Number.java
@@ -0,0 +1,30 @@
+/*Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.*/
+
+class Palindrome_Number {
+public:
+    bool isPalindrome(int x) {
+         if (x < 0) {
+            return false;
+        }
+        // Store the number in a variable
+        int number = x;
+        // This will store the reverse of the number
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return x == reverse;
+    }
+    
+};
